722ccf5532c46c311871a9f203e11f857050aa27
Fatemeh-Alizadeh <82482430+Fatemeh-Alizadeh@users.noreply.github.com>
2021-08-15 15:34:34 +1000

    finish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import {ImQuotesRight, ImCircleRight, ImCircleLeft ,ImTwitter } from "react-icon
 
 function App() {
     const [index, setIndex] = useState(0);
-    const {text, author, color } = quote[index];
+    const { text, author, color } = quote[index];
+
+    const checkNumber = (num) => {
+        if (num > quote.length - 1) {
+            return 0
+        }
+        if (num < 0){
+          return  quote.length - 1
+        }
+        else {
+            return num;
+        }
+    }
+
+    const nextQuote = () => {
+        setIndex((index) => {
+            let newIndex = index + 1
+            return checkNumber(newIndex)
+        })
+    }
+     const preQuote = () => {
+        setIndex((index) => {
+            let newIndex = index - 1
+            return checkNumber(newIndex)
+        })
+    }
 
     return (
         
@@ -20,8 +45,8 @@ function App() {
                     <h6>-{author}</h6>
                 </div>
                 <a href="twitter.com/intent/tweet" id="tweet-quote"><ImTwitter /></a>
-                <button id='new-quote'><ImCircleLeft /></button>
-                <button id='pre-quote'><ImCircleRight /></button>
+                <button id='new-quote' onClick={nextQuote}><ImCircleLeft /></button>
+                <button id='pre-quote' onClick={preQuote}><ImCircleRight /></button>
             </div>
 
     </section>
